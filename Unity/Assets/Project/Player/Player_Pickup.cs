d3479bb036636808a06d8c784f07c4df47f79f1a
Unknown <Isaacboon@gmail.com>
2018-07-30 15:23:08 +1200

    Player  Pickup
    Can pick up the one cube

---
+++ Unity/Assets/Project/Player/Player_Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Pickup : MonoBehaviour {
+
+    public GameObject testCube;
+    public GameObject pickupLocation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            testCube.transform.position = pickupLocation.transform.position;
+            testCube.transform.rotation = pickupLocation.transform.rotation;
+            testCube.GetComponent<Rigidbody>().isKinematic = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            testCube.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+	}
+
+}

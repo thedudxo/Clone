24cc6ec67dec78617d8abc467c30425fbe874023
Unknown <Isaacboon@gmail.com>
2018-08-08 15:23:46 +1200

    Assets in unity
    Door + button + cube

---
+++ UnityNewPickup/Assets/Project/General_Types/Grabbable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : MonoBehaviour {
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Player_Pickup.Instance.carriedObject != null) {
+            Player_Pickup.Instance.dropObject();
+        }
+    }
+}

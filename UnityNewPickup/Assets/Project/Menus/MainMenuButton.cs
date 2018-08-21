7fc44b5f1f42803f1ceccb695a04685e185d12fe
Jayne Gale <jaynegale@gmail.com>
2018-08-21 15:27:10 +1200

    MenusPartlyDone
    Scripts on but Buttons not working

---
+++ UnityNewPickup/Assets/Project/Menus/MainMenuButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour {
+
+    public void ClonePlay()
+    {
+ //       SceneManager.LoadScene("Master Scene");
+        SceneManager.LoadScene("External_Final");
+    }
+
+    public void CloneControls()
+    {
+        SceneManager.LoadScene("ControlsMenu");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void CloneQuit()
+    {
+        Application.Quit();
+    }
+}

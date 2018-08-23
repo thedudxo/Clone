4ff9d12f985f4d0c72cc40f62c910e9b076dd29e
Jordan <jordanclark96@hotmail.com>
2018-08-23 15:26:21 +1200

    Most Audio Working

--- UnityNewPickup/Assets/Project/Audio/AudioManager.cs
+++ UnityNewPickup/Assets/Project/Audio/AudioManager.cs
@@ -52,4 +52,10 @@ public class AudioManager : MonoBehaviour
         s.source.Play();
 
     }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }
 }

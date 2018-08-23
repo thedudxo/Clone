756877d6b11d1df5a838e577ed74b6689b739b74
Jayne Gale <jaynegale@gmail.com>
2018-08-23 22:13:18 +1200

    Orbiting rocks and islands
    External environment scripts

---
+++ UnityNewPickup/Assets/Project/Environment/Scripts/SlowOrbit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowOrbit : MonoBehaviour
+{
+    public float orbitSpeed;
+    public Vector3 centreOfOrbit;
+    void Update()
+        {
+            transform.RotateAround(centreOfOrbit, Vector3.up, orbitSpeed * Time.deltaTime);
+        }
+    }
---
+++ UnityNewPickup/Assets/Project/Environment/Scripts/Spin.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+    public Vector3 rotationSpeed;
+void Update () {
+    gameObject.transform.eulerAngles += rotationSpeed;
+    }
+}

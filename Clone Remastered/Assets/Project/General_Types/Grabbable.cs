be548d88b5fe95603902ac2dbaf1a4aff429e977
Jordan <jordanclark.96@hotmail.com>
2019-04-18 12:25:34 +1200

    Remaster

---
+++ Clone Remastered/Assets/Project/General_Types/Grabbable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : MonoBehaviour {
+
+    private Vector3 spawnPostion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Player_Pickup.Instance.carriedObject != null) {
+            Player_Pickup.Instance.dropObject();
+        }
+    }
+    
+    private void Start()
+    {
+        spawnPostion = transform.position;
+    }
+
+    public void Reset()
+    {
+        GetComponent<ParticleSystem>().Emit(40);
+
+        transform.position = spawnPostion;
+        if(Player_Pickup.Instance.carriedObject == this.gameObject)
+        {
+            Player_Pickup.Instance.dropObject();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player_Pickup.Instance.carriedObject.GetComponent<MeshRenderer>().material = Player_Pickup.Instance.holoError;
+        Player_Pickup.Instance.drop = false;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Player_Pickup.Instance.carriedObject.GetComponent<MeshRenderer>().material = Player_Pickup.Instance.hologram;
+        Player_Pickup.Instance.drop = true;
+    }
+}
---
+++ Clone Remastered/Assets/Project/Puzzle_Pieces/Button_Behaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Behaviour : MonoBehaviour {
+
+    public bool isPressed = false;
+    public int weights = 0;
+    public Animator ButtonAnim;
+
+    private static Button_Behaviour instance;
+
+    public static Button_Behaviour Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<Button_Behaviour>();
+            }
+            return Button_Behaviour.instance;
+        }
+    }
+
+    private void Update()
+    {
+        if(weights > 0)
+        {
+            isPressed = true;
+        }
+        else
+        {
+            isPressed = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Weighted>() != null){
+            ButtonAnim.SetBool("ButtonPress", true);
+            weights++;
+            isPressed = true;
+
+            if(collision.gameObject.GetComponent<Cloneable>() != null)
+            {
+                collision.gameObject.GetComponent<Cloneable>().setButton(gameObject);
+            }
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Weighted>() != null)
+        {
+            weights--;
+            if (weights == 0) {
+                ButtonAnim.SetBool("ButtonPress", false);
+                isPressed = false;
+            }
+
+            if (collision.gameObject.GetComponent<Cloneable>() != null)
+            {
+                collision.gameObject.GetComponent<Cloneable>().setButton(null);
+            }
+        }
+    }
+  
+}

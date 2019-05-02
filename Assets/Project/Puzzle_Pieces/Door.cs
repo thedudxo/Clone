9af826f2a1f5db65fcd25033f1b957182349462e
Isaac <Isaacboon@gmail.com>
2019-05-02 12:28:59 +1200

    Removed old project, added button

---
+++ Assets/Project/Puzzle_Pieces/Door.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour{
+
+    [SerializeField] private GameObject[] buttons;
+    
+    public Animator doorOpenAnimation;
+    
+    void Start () {
+        foreach(GameObject button in buttons)
+        {
+            if (button.GetComponent<Button_Behaviour>() == null)
+            {
+                throw new MissingComponentException();
+            }
+        }
+		
+	}
+	
+	void Update () {
+        int buttonsPressed = 0;
+        foreach (GameObject button in buttons)
+        {
+            if (button.GetComponent<Button_Behaviour>().isPressed)
+            {
+                buttonsPressed++;
+            }
+        }
+
+        if (buttonsPressed >= buttons.Length)
+        {
+            doorOpenAnimation.SetBool("ButtonPush", true);
+        }
+        else
+        {
+            doorOpenAnimation.SetBool("ButtonPush", false);
+        }
+    }
+}

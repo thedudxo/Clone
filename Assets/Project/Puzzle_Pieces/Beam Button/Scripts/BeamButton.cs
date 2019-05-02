55a1764e4f3e0d57c5203d524b8f56223071bb25
Isaac <Isaacboon@gmail.com>
2019-05-02 14:30:42 +1200

    button prefab mostly done. arrow changes colour

---
+++ Assets/Project/Puzzle_Pieces/Beam Button/Scripts/BeamButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamButton : MonoBehaviour {
+
+    int cubes = 0;
+    public Material greenArrow;
+    public Material redArrow;
+    public GameObject arrow;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void checkArrow()
+    {
+        if (cubes > 0)
+        {
+            arrow.GetComponent<Renderer>().material = greenArrow;
+        }
+        else
+        {
+            arrow.GetComponent<Renderer>().material = redArrow;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("yesh");
+        if (other.GetComponent<Weighted>())
+        {
+            cubes++;
+            checkArrow();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Weighted>())
+        {
+            cubes--;
+            checkArrow();
+        }
+    }
+}
---
+++ Assets/Project/Puzzle_Pieces/Beam Button/Scripts/FacePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //face the player
+        transform.LookAt(target);
+
+        // stop rotating upwards
+        var  rotate = transform.eulerAngles; rotate.x = 0;
+        transform.rotation = Quaternion.Euler(rotate);
+    }
+}

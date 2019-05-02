e569e5776f671f51414db9869bc86d132e0b118d
thedudxo <Isaacboon@gmail.com>
2019-05-03 10:42:54 +1200

    Bare minimum button/door functionality

--- Assets/Project/Player/Scripts/New/CamMouseLook.cs
+++ Assets/Project/Player/Scripts/New/CamMouseLook.cs
@@ -16,7 +16,7 @@ public class CamMouseLook : MonoBehaviour {
 	void Update () {
 		RotateCamera();
         if(transform.rotation.eulerAngles.x >= 50 && transform.rotation.eulerAngles.x <= 269 && playerBody.GetComponent<Player_Pickup>().carrying) { playerBody.GetComponent<Player_Pickup>().Drop(); }
-        Debug.Log(transform.rotation.eulerAngles.x);
+        //Debug.Log(transform.rotation.eulerAngles.x);
 	}
 	
 	

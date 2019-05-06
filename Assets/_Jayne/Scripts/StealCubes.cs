b0e8ed2e4b30d131a92cc5b2612ccc90a44d0dcc
Jayne Gale <jaynegale@gmail.com>
2019-05-06 22:32:26 +1200

    As working as I can get it
    Cubes won't stay in button, especially more than one
    Doors not animating
    Yet to code Hampsters to steal the right cubes 
    Player floating
    No crosshair visible
    Pickup and cloning working, incl clone animation

---
+++ Assets/_Jayne/Scripts/StealCubes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealCubes : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            EwokDestinations.goPickupCube = true;
+        }
+    }
+}

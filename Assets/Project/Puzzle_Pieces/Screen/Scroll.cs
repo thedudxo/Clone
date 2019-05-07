5866ba2821384de0c6b27f48976db7d334377427
Isaac <Isaacboon@gmail.com>
2019-05-07 15:00:15 +1200

    Retrofuturistic

---
+++ Assets/Project/Puzzle_Pieces/Screen/Scroll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+    public float startPos;
+    public float endPos;
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float newY = transform.position.y + speed;
+
+        if (transform.position.y < endPos)
+        {
+            newY = startPos;
+        }
+
+        transform.position = new Vector3(
+                    transform.position.x,
+                    newY,
+                    transform.position.z);
+    }
+}

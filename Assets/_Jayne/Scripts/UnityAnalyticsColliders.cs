295f812b1b8f70a72d9ab61e2ddc80fd7a2f9fc8
Jayne Gale <jaynegale@gmail.com>
2019-05-27 00:41:44 +1200

    Analytics Colliders

---
+++ Assets/_Jayne/Scripts/UnityAnalyticsColliders.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class UnityAnalyticsColliders : MonoBehaviour
+{
+    public string colliderName;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            Analytics.CustomEvent(colliderName);
+        }
+    }
+}

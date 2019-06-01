0c280c56a91a18a2dd95ec65e52e1f9f1eb57b2c
thedudxo <Isaacboon@gmail.com>
2019-06-01 12:48:01 +1200

    Part1 Anaylitics

---
+++ UnityNewPickup/Assets/UnityAnalyticsColliders.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class UnityAnalyticsColliders : MonoBehaviour
+{
+    public string colliderName;
+    public bool doDisable = true;
+    private bool disabled = false;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player" && !disabled)
+        {
+            Analytics.CustomEvent(colliderName);
+
+            if (doDisable)
+            {
+                disabled = true;
+            }
+        }
+    }
+}

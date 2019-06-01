83e50df739b21ea50e181e18872f95104a1d09f6
thedudxo <Isaacboon@gmail.com>
2019-06-01 16:03:39 +1200

    ungrabbable cubes D:

--- Assets/Project/General_Types/Grabbable.cs
+++ Assets/Project/General_Types/Grabbable.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Grabbable : MonoBehaviour {
 
-    public Collider dropMe;
+    Collider playerCarryCollider;
     public GameObject player;
     public bool carried = false;
 
+    private void Start()
+    {
+        playerCarryCollider = PlayerManager.player_Pickup.carrier.GetComponent<SphereCollider>();
+    }
+
     private void OnTriggerExit(Collider other) {
-        if (other == dropMe && gameObject == PlayerManager.player_Pickup.carriedObject) {
+        if (other == playerCarryCollider && gameObject == PlayerManager.player_Pickup.carriedObject) {
             PlayerManager.player_Pickup.Drop();
         }
     }
